fd1eb02fc0764faf3717482e686d66ef32bc9d6d
nfujita0707 <nfujita0714@gmail.com>
2021-09-03 16:45:41 +0000

    表示変更

--- app/controllers/estates_controller.rb
+++ app/controllers/estates_controller.rb
@@ -8,37 +8,36 @@ class EstatesController < ApplicationController
 
   # GET /estates/1 or /estates/1.json
   def show
+    @estate = Estate.find(params[:id])
+    @stations = @estate.stations
   end
 
   # GET /estates/new
   def new
     @estate = Estate.new
+    2.times {@estate.stations.build}
   end
 
   # GET /estates/1/edit
   def edit
+    @estate.stations.build
+    @estates = Station.all
   end
 
-  # POST /estates or /estates.json
   def create
-    @estate = Estate.new(estate_params)
-
-    respond_to do |format|
-      if @estate.save
-        format.html { redirect_to @estate, notice: "Estate was successfully created." }
-        format.json { render :show, status: :created, location: @estate }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @estate.errors, status: :unprocessable_entity }
-      end
-    end
+  @estate = Estate.new(estate_params)
+  if @estate.save
+    flash[:notice] = "登録が完了しました！"
+    redirect_to estate_path(@estate.id)
+  else
+    render :new
   end
+end
 
-  # PATCH/PUT /estates/1 or /estates/1.json
   def update
     respond_to do |format|
       if @estate.update(estate_params)
-        format.html { redirect_to @estate, notice: "Estate was successfully updated." }
+        format.html { redirect_to @estate, notice: "正常に更新されました！" }
         format.json { render :show, status: :ok, location: @estate }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -47,23 +46,20 @@ class EstatesController < ApplicationController
     end
   end
 
-  # DELETE /estates/1 or /estates/1.json
   def destroy
     @estate.destroy
     respond_to do |format|
-      format.html { redirect_to estates_url, notice: "Estate was successfully destroyed." }
+      format.html { redirect_to estates_url, notice: "情報を削除しました！" }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_estate
+  def set_estate
       @estate = Estate.find(params[:id])
-    end
+  end
 
-    # Only allow a list of trusted parameters through.
-    def estate_params
-      params.require(:estate).permit(:name, :price, :adress, :year, :content)
-    end
+  def estate_params
+      params.require(:estate).permit(:name, :price, :adress, :year, :content, stations_attributes: [:route, :station_name, :walk, :estate_id])
+  end
 end
